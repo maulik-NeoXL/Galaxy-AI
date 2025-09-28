528a23c90e963e6bb9c39fb89bd1ad5232931f12
maulik-NeoXL <maulik@neoxl.com>
2025-09-29 02:43:45 +0530

    Fix MongoDB type compatibility for deployment

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -13,8 +13,8 @@ if (!MONGODB_URI) {
  */
 declare global {
   var mongoose: {
-    conn: typeof mongoose | null;
-    promise: Promise<typeof mongoose> | null;
+    conn: any;
+    promise: any;
   };
 }
 
